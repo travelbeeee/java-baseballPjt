7bd508ceb3fe89a1b8fd68a5f39c8c67981ad95f
travelbeeee <sochun1518@naver.com>
2021-06-29 13:38:38 +0900

    사용자의 입력에 따라 게임을 시작하고 끝내는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,32 @@
 package baseball;
 
+import utils.RandomUtils;
+
 import java.util.Scanner;
 
 public class Application {
+
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
+        initGame(scanner);
+
+        return;
+    }
+
+    private static void initGame(Scanner scanner){
+        System.out.println("안녕하세요, 숫자 야구 게임입니다.");
+        while(true){
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            int doGameOrNot = scanner.nextInt();
+            if(doGameOrNot == 2)
+                break;
+            doGame(scanner);
+        }
+        return;
+    }
+
+    private static void doGame(Scanner scanner) {
+
     }
 }
